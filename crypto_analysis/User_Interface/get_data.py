f7b6678ddd45b5f3c8c615ba5a18d758b92d5c18
Tatjana Hoesch <tatjana@hoesch-family.de>
2022-07-18 12:31:50 +0200

    committing because of new requirements

--- crypto_analysis/User_Interface/get_data.py
+++ crypto_analysis/User_Interface/get_data.py
@@ -8,9 +8,10 @@ def load_data(ticker=None, timeframe='5y'):
     '''
     This function loads stcks data per API call.
     '''
+    print(ticker)
     data = yf.download(ticker, period=timeframe)
     data.reset_index(inplace=True)
-
+    print(data)
     df_stocks.set_index('Date', inplace=True)
     df_stocks = df_stocks['Open']
     df_stocks = df_stocks.reset_index(level=0)
@@ -23,13 +24,8 @@ def load_data(ticker=None, timeframe='5y'):
 
 
 
-
-
-
-
-
 if __name__ == "__main()__":
-
+    print("=====starting=====")
     # variables
     timeframe = '5y'
     stocks = ('^GSPC', '^IXIC', '000001.SS', '^N100', '^NDX')
